7ba22f2399156cf8af193832ae043085ccaa2b98
krit <krittapasboonruang@gmail.com>
2025-12-18 21:35:25 +0700

    lab8_3 complete

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,33 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int age, height;
+    long long bounty;
+    string name;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age <= 25) {
+        cout << "Enter your height: ";
+        cin >> height;
+        if (height < 100) name = "Chopper";
+        else if (height < 180) name = "Usopp";
+        else {
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            name = (bounty > 1100000000) ? "Zoro" : "Sanji";
+        }
+    } else if (age <= 60) {
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        name = (bounty > 500000000) ? "Jinbe" : "Franky";
+    } else {
+        name = "Brook";
+    }
+
+    cout << "Your character = " << name;
+    return 0;
+}
